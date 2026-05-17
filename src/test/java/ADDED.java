ec2a42b7408a493c7fd4661a89f1e7b1c6eff96c
Anandsaz <anand,sag1994@gmail.com>
2026-05-17 16:56:18 +0530

    change 2 for feature ANAND

---
+++ src/test/java/ADDED.java
@@ -0,0 +1,20 @@
+import com.microsoft.playwright.*;
+
+import java.util.Arrays;
+
+public class ADDED {
+    public static void main(String[] args) {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)
+                .setArgs(Arrays.asList("--start-maximized")));
+        BrowserContext context = browser.newContext(new Browser.NewContextOptions().setViewportSize((null)));
+        Page page = context.newPage();
+        System.out.println("new rebase line added");
+        System.out.println("new Anand line added");
+        page.navigate("https://testautomationpractice.blogspot.com/");
+
+        System.out.println("new line added");
+        System.out.println("new line added");
+    }
+}
