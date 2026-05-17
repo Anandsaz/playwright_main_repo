3a7aa71f7f930bbf8d81b622bd627712d9fa1b7b
Anandsaz <anand,sag1994@gmail.com>
2026-05-17 15:54:51 +0530

    change 2 for feature ANAND

--- src/test/java/NEW_ADDED.java
+++ src/test/java/NEW_ADDED.java
@@ -10,7 +10,7 @@ public class NEW_ADDED {
                 .setArgs(Arrays.asList("--start-maximized")));
         BrowserContext context = browser.newContext(new Browser.NewContextOptions().setViewportSize((null)));
         Page page = context.newPage();
-        System.out.println("new Anand line added");
+        System.out.println("new rebase line added");
         System.out.println("new Anand line added");
         page.navigate("https://testautomationpractice.blogspot.com/");
 
